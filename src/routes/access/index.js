a5763fe174e4009882cdcf82633ee89e9eced874
httngan <huynhthanhngan1712@gmail.com>
2023-12-15 10:54:59 +0700

    no message

--- src/routes/access/index.js
+++ src/routes/access/index.js
@@ -4,12 +4,16 @@ const express = require('express');
 const accessController = require('../../controllers/access.controller');
 const router = express.Router();
 const { asyncHandler } = require('../../auth/checkAuth')
+const { authentication } = require('../../auth/authUtils')
 
-router.post('/shop/signup', accessController.signUp)
-router.post('/shop/login', accessController.login)
+router.post('/shop/signup', asyncHandler(accessController.signUp))
+router.post('/shop/login', asyncHandler(accessController.login))
 
 ///authentication
 
-router.post('/shop/authentication', accessController.logout)
+router.post('/shop/logout', asyncHandler(accessController.logout))
+
+router.use(authentication)
+
 
 module.exports = router
